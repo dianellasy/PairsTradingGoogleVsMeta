b012bef1624b4b6a8d69829acf2de385cc044590
dianella ˚ʚ♡ɞ˚ <143135166+dianellasy@users.noreply.github.com>
2025-06-05 15:01:06 -0700

    Import Google and Meta CSV Files

--- pairs_trading.py
+++ pairs_trading.py
@@ -1,4 +1,7 @@
 import csv
 
-with open('', newline='') as csvfile:
-    reader = csv.reader(csvfile)
+with open('google_stocks.csv', newline='') as csvfile1:
+    google_stocks_reader = csv.reader(csvfile1)
+
+with open('meta_stocks.csv', newline='') as csvfile2:
+    meta_stocks_reader = csv.reader(csvfile2)
